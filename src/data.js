087008c6902ce5cc03e5d9715967e90caaf64c68
Naomi Peña <naosan25@protonmail.com>
2023-01-02 15:25:03 -0600

    Feat:Función para mostrar datos de atletas

--- src/data.js
+++ src/data.js
@@ -1,9 +1,40 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+import olympics from "./data/athletes/olympics.js";
+import athletes from "./data/athletes/olympics.js";
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//const teamList = athletes.team;  //Arrojó undefined en console.log
+
+//const athletesList = athletes; //Esta variable si la aceptó pero al usarla en la funcion nos apareció athletesList[object Object] en el console.log
+
+//console.log(JSON.stringify(athletesList)); de esta manera podemos visualizar todos los datos dentro de athletesList en la consola
+//const noc = athletes.noc; Arrojó undefined en console.log
+
+//const searchCountry = document.querySelector(".searchCountries");
+//const country = document.querySelector(".country");
+//searchCountry.addEventListener('keypress', countries);
+
+
+//function countries () {
+  //const container= document.querySelector("#tableCountry");
+  //const nameOfTheCountry = country.value;
+  //for (let i = 0; i <= olympics.athletes.length; i++) {
+    //const countriesTable = olympics.athletes[5].value;//antes[i].team;
+    //const abreviationNocTable = olympics.athletes[6].value;//antes[i].noc;
+    //let nAthletes = document.querySelector(".athletesNumber");
+    
+    //if (searchCountry === countriesTable || searchCountry === abreviationNocTable)  {
+      //countriesFunction.textContent = athletes.team;
+      //nameOfTheCountry.textContent = olympics.athletes[5].value;
+      // como asignar a mi tabla/caja de texto el valor de athletes[i].PROPIEDADAUSAR
+    //} 
+  //} 
+//}
+  //console.log ("countries" + countries)
+  //console.log ("countriesFunction" + countries)
+
+//Creando funciones para el html de atletas
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,45 @@
-import { example } from './data.js';
+import olympics from "./data/athletes/olympics.js";
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//const athletesArray= athletes;
+const searchAthletesBtn = document.querySelector(".searchBtn");
+const inputName = document.querySelector("#inputAthletes");
+const showName = document.querySelector("#nameTag");
+const showCountry = document.querySelector("#countryTag"); 
+const showSport = document.querySelector("#sportTag");
+const showEvent = document.querySelector("#eventTag");
+const showHeight = document.querySelector("#heightTag");
+const showWeight = document.querySelector("#weightTag")
+const showMedal = document.querySelector("#medalTag");
+const searchButton = document.querySelector(".searchBtn");
 
-console.log(example, data);
+
+let athleteItem = 0
+
+window.addEventListener("DOMContentLoaded", function(){
+  const selectAthlete = olympics.athletes[athleteItem];
+  showName.textContent = selectAthlete.name;
+  showCountry.textContent = selectAthlete.team;
+  showSport.textContent = selectAthlete.sport;
+  showEvent.textContent = selectAthlete.event;
+  showHeight.textContent = selectAthlete.height;
+  showWeight.textContent = selectAthlete.weight;
+  showMedal.textContent = selectAthlete.medal;
+});
+
+function showPerson(person) {
+  const selectAthlete = olympics.athletes[person];
+  showName.textContent = selectAthlete.name;
+  showCountry.textContent = selectAthlete.team;
+  showSport.textContent = selectAthlete.sport;
+  showEvent.textContent = selectAthlete.event;
+  showHeight.textContent = selectAthlete.height;
+  showWeight.textContent = selectAthlete.weight;
+  showMedal.textContent = selectAthlete.medal;
+}
+
+searchButton.addEventListener("click", function (){
+  console.log("El botón funciona");
+  athleteItem = Math.floor(Math.random()*olympics.athletes.length);
+  showPerson(athleteItem); });
+
+//console.log(example, data);
